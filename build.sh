877ff517e12af5fd985d5e6f34d75143871a0047
Cesar Denis <cesar@cesardenis.com>
2017-08-07 17:29:05 -0300

    Initial travis setup

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+## only process script when started not by pull request (PR)
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# build site with jekyll
+bower install
+gulp build --prod
+gulp upload
